7766a51b7792a20d81e9af0a5ab24a35b16339b2
alexkuvshinoff <45110174+alexkuvshinoff@users.noreply.github.com>
2020-06-09 20:40:51 -0400

    Create get_data.py

---
+++ get_data.py
@@ -0,0 +1,17 @@
+# get_data.py
+import requests
+import json
+
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+#----- part 1 --------
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+
+response = requests.get(request_url)
+response_data = json.loads(response.text)
+
+print(response_data['department'],":",response_data['name'],",$",response_data['price'])
+
+#------- part 2 -------
+
