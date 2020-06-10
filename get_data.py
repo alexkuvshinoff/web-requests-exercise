bf35ef9a3ceb6785e26d8c1bf029ec1ad66b1ee7
alexkuvshinoff <45110174+alexkuvshinoff@users.noreply.github.com>
2020-06-09 20:52:01 -0400

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -15,3 +15,12 @@ print(response_data['department'],":",response_data['name'],",$",response_data['
 
 #------- part 2 -------
 
+request2_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products.json"
+
+response = requests.get(request_url)
+response_data = json.loads(response.text)
+
+for x in response_data2:
+        print(x["id"], x["name"]
+
+# ---------
